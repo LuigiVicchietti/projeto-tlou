fd019237e0cba9449e0c6dcda364991f08cb5fc5
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-06-03 00:14:57 -0300

    Criação de um botão q gera música aleatórias (simples)

--- projeto-tlou/public/js/script.js
+++ projeto-tlou/public/js/script.js
@@ -1,22 +1,25 @@
-const // navItens = document.querySelectorAll(".nav-item"),
-    // titleNavbar = document.getElementById("title-navbar"),
-    // firefly = document.getElementById("firefly-navbar"),
-        navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 
 const ativarScroll = () => {
     navbar.classList.toggle("alt", scrollY > 0);
+}
 
-    // navItens.forEach(e => {
-    //     e.classList.toggle("alt", scrollY > 0);
-    // })
+var musics = [`./assets/audios/TheLastOfUs-themeSong.mp3`, `./assets/audios/theLastOfUs-part2-themeSong.mp3`, `./assets/audios/allGoneAftermatch.mp3`,
+    `./assets/audios/thePathNewBeggining.mp3`, `./assets/audios/grieving.mp3`, `./assets/audios/allowedToBeHappy.mp3`, `./assets/audios/throughTheValley.mp3`]
 
-    // if (scrollY === 0) {
-    //     firefly.setAttribute("src", "https://static.tvtropes.org/pmwiki/pub/images/fireflies.png")
-    //     titleNavbar.setAttribute("src", "https://gamerview.uai.com.br/wp-content/uploads/2020/06/the-last-of-us-part-2-logo.png")
-    // } else {
-    //     firefly.setAttribute("src", "https://i.pinimg.com/originals/7b/06/34/7b0634d5c9a609b047e9cdb3ac0668c4.png")
-    //     titleNavbar.setAttribute("src", "https://imagensemoldes.com.br/wp-content/uploads/2021/06/Logo-The-Last-of-Us-PNG.png")
-    // }
+function playMusic() {
+    let  flexibleSpan = document.getElementById(`flexible_span`);
+
+    let numRandom = parseInt(Math.random() * musics.length);
+    console.log(numRandom)
+    let audio = document.querySelector(`audio`);
+
+    audio.setAttribute(`src`, `${musics[numRandom]}`)
+    flexibleSpan.classList.add(`clicked`);
+    setTimeout(() => { flexibleSpan.style.display = `none` }, 501);
+    setTimeout(() => { document.querySelector(`.loader`).style.display = `flex` }, 502);
+    setTimeout(() => { document.querySelector(`.loader`).classList.add (`show`) }, 505);
+    audio.play();
 }
 
 window.addEventListener('scroll', ativarScroll);
